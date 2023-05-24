35919f4a8639bf7e34783c8fa3bc845d89020f14
Douglas Oliveira <dougmoroli@gmail.com>
2023-05-24 15:59:34 -0300

    usando rotas aninhadas

---
+++ src/componentes/PaginaPadrao/index.jsx
@@ -0,0 +1,13 @@
+import Banner from "componentes/Banner";
+import { Outlet } from "react-router-dom";
+
+export default function PaginaPadrao() {
+    return (
+        <main>
+            <Banner />
+
+            {/* Renderizar conteúdo da rota aqui... */}
+            <Outlet />
+        </main>
+    );
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,29 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/Sobre Mim";
 import Menu from "componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Menu />
             <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/sobremim" element={<SobreMim />} />
+                <Route path="/" element={<PaginaPadrao />}>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/sobremim" element={<SobreMim />} />
+                </Route>
+                {/* Demonstração do uso de <Outlet / no index.jsx da <PaginaPadra />>
+                
+                    Na rota "/", a estrutura a ser renderizada é:
+                    <PaginaPadrao>
+                        <Inicio />
+                    <PaginaPadrao />
+
+                    Na rota "/sobremim", a estrutura a ser renderizada é:
+                    <PaginaPadrao>
+                        <Sobremim />
+                    <PaginaPadrao />
+                */}
                 <Route path="*" element={<div>Página não encontrada</div>} />
             </Routes>
             <Rodape />
