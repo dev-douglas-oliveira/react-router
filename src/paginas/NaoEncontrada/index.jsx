1e5af5f3dde3e41545635d34769c712180a41d7f
Douglas Oliveira <dougmoroli@gmail.com>
2023-05-25 19:35:06 -0300

    utilizando useNavigate

--- src/paginas/NaoEncontrada/index.jsx
+++ src/paginas/NaoEncontrada/index.jsx
@@ -1,7 +1,11 @@
 import styles from "./NaoEncontrada.module.css";
 import erro404 from "../../assets/erro_404.png";
+import BotaoPrincipal from "componentes/BotaoPrincipal";
+import { useNavigate } from "react-router-dom";
 
 export default function NaoEncontrada() {
+    const navegar = useNavigate();
+
     return (
         <>
             <div className={styles.conteudoContainer}>
@@ -14,9 +18,12 @@ export default function NaoEncontrada() {
                     Aguarde uns instantes e recarregue a página, ou volte para a
                     página inicial.
                 </p>
-
-                <div className={styles.botaoContainer}>
-                    <button>Voltar</button>
+                {/* navegar(-1) volta para a página anterior que está sendo navegada */}
+                <div
+                    className={styles.botaoContainer}
+                    onClick={() => navegar(-1)}
+                >
+                    <BotaoPrincipal tamanho="lg">Voltar</BotaoPrincipal>
                 </div>
 
                 <img
