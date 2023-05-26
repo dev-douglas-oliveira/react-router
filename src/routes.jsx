86d98ea785efb764efeba0aeeda25fb359908e87
Douglas Oliveira <dougmoroli@gmail.com>
2023-05-26 10:03:11 -0300

    implementando posts recomendados e usando ScrollToTop

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,17 +6,19 @@ import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
 import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
+            <ScrollToTop />
             <Menu />
             <Routes>
                 <Route path="/" element={<PaginaPadrao />}>
                     <Route index element={<Inicio />} />
                     <Route path="sobremim" element={<SobreMim />} />
                 </Route>
-                <Route path="posts/:id" element={<Post />} />
+                <Route path="posts/:id/*" element={<Post />} />
                 {/* Demonstração do uso de <Outlet / no index.jsx da <PaginaPadra />>
                 
                     Na rota "/", a estrutura a ser renderizada é:
